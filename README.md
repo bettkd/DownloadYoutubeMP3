dcf5854224c057063d6d693da5cd79a0d8410325
Dominic Bett <dominicbett@hotmail.com>
2016-01-12 22:19:11 -0500

    Downloads from a list

--- README.md
+++ README.md
@@ -7,3 +7,16 @@ To install dependancies, run the following on the terminal
 $sudo pip install -r requirements.txt
 ```
 
+### Example tab-delimited text file ```playlist.txt```
+|Artist          | Song                              |
+|:---------------|:----------------------------------|
+|MercyMe         | Greater                           |
+|Newsboys        | We Believe                        |
+|MercyMe         | I Can Only Imagine                |
+|Chris Tomlin    | Amazing Grace (My Chains Are Gone)|
+|Matt Redman     | "10,000 Reasons"                  |
+|Newsboys        | God's Not Dead                    |
+|Chris Tomlin    | How Great Is Our God              |
+|MercyMe         | Word ofGod Speak                  |
+|Casting Crowns  | Who Am I                          |
+|Matthew West    | "Hello, My Name Is"               |
--- downloadmp3.py
+++ downloadmp3.py
@@ -2,6 +2,7 @@ from __future__ import unicode_literals
 import youtube_dl
 import csv
 from parse_youtube import getURI
+import os
 
 YOUTUBE_URL = "https://youtube.com"
 
@@ -9,13 +10,42 @@ def getVideoURLs(fname):
 	videoURLs = []
 	playlist = csv.reader(open(fname, 'rU'), dialect=csv.excel_tab)
 	for row in playlist:
-		artist, song = row
-		query = "/results?search_query="+artist.strip().replace(" ", "+")+"+-+"+song.strip().replace(" ", "+")
-		URI = getURI(YOUTUBE_URL + query)
-		videoURL = YOUTUBE_URL + URI
-		print videoURL
-		videoURLs.append(videoURL)
+		artist, title = row
+		videoURLs.append(getVideoURL(artist, title))
+		break
 	return videoURLs
 
-fname = "playlist.txt"
-getVideoURLs(fname)
+def getVideoURL(artist, title):
+	query = "/results?search_query="+artist.strip().replace(" ", "+")+"+-+"+title.strip().replace(" ", "+")
+	print ("\nNow loading %s by %s..."%(title, artist))
+	URI = getURI(YOUTUBE_URL + query)
+	videoURL = YOUTUBE_URL + URI
+	return videoURL
+
+def downloadMusic(videoURLs, downloadPath="~/Downloads/YoutubeDownload/"):
+	if not os.path.exists(os.path.dirname(downloadPath)):
+		os.makedirs(os.path.dirname(downloadPath))
+	ydl_opts = {
+		'outtmpl': os.path.join(downloadPath, '%(title)s.%(ext)s'),
+		'nocheckcertificate': True,
+		'noplaylist' : True,
+		'format': 'bestaudio/best',
+		'postprocessors': [{
+			'key': 'FFmpegExtractAudio',
+			'preferredcodec': 'mp3',
+			'preferredquality': '192',
+		}],
+	}
+	with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+		ydl.download(videoURLs)
+
+def main():
+	fname = "playlist.txt"
+	videoURLs = getVideoURLs(fname)
+	downloadMusic(videoURLs)
+
+	print ("Done!")
+
+
+if __name__ == '__main__':
+	main()
